7c748ff06d7eae208159cff640d8749d22c42c98
Kettil Wigenstedt <kettil.brochmann@gmail.com>
2020-11-03 11:45:42 +0100

    Changed main.c to continuously print if there's been a change in the button.

--- main.c
+++ main.c
@@ -2,19 +2,24 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <stdio.h>
+#include <util/delay.h>
+#include <stdbool.h>
 
 #include "led.h"
 #include "serial.h"
 #include "timer.h"
+#include "button.h"
 
-void main (void) {
+int main (void) {
 
 	LED_init();
 	uart_init();
 	timer_init();
+	button_init();
 
 	while (1) {
-		// ...
+		print_button_state();
 	}
+	return 0;
 }
 
