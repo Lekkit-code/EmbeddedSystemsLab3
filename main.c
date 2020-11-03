03906771fd692148c0092fd8d4120f4dc061bcce
Kettil Wigenstedt <kettil.brochmann@gmail.com>
2020-11-03 19:45:54 +0100

    Changed main.c to accomodate states.

--- main.c
+++ main.c
@@ -12,8 +12,14 @@
 #include "timer.h"
 #include "button.h"
 #include "potentiometer.h"
+#include "state.h"
+
+#define WAITING_TIME 1000
 
 volatile uint16_t adc_value = 0;
+volatile uint32_t ten_millis = 0;
+uint32_t previous_millis = 0;
+volatile enum STATE s = PULSE;
 
 int main (void) {
 
@@ -25,19 +31,23 @@ int main (void) {
 	sei();
 
 	while (1) {
+		handle_LED(s, ten_millis);
 	}
 	return 0;
 }
 
 ISR(TIMER2_COMPA_vect) //When the interrupt triggers, updates the brightness of the LED, starts a new ADC-conversion and clears the interrupt flag.
 {
-	OCR0A = adc_value;
+	s = button_check(s);
+	OCR0A = simple_ramp();
+	ten_millis++;
 	ADCSRA |= (1 << ADSC);
 	TIFR2 &= ~(1 << OCF2A);
 }
 
 ISR(ADC_vect) //When the interrupt triggers because an ADC-conversion is complete, updates the read value.
 {
+	OCR0A = adc_value;
 	adc_value = ADCH;
 	ADCSRA &= ~(1 << ADIF);
 }
