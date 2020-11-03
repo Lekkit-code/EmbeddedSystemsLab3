d7988d3de9dc9d78946bb1e7367dbe06e0892c7e
Kettil Wigenstedt <kettil.brochmann@gmail.com>
2020-11-03 14:45:26 +0100

    Changed main.c to adjust the brighness of the LED according to the potentiometer.

--- main.c
+++ main.c
@@ -4,11 +4,16 @@
 #include <stdio.h>
 #include <util/delay.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
 
 #include "led.h"
 #include "serial.h"
 #include "timer.h"
 #include "button.h"
+#include "potentiometer.h"
+
+volatile uint16_t adc_value = 0;
 
 int main (void) {
 
@@ -16,10 +21,23 @@ int main (void) {
 	uart_init();
 	timer_init();
 	button_init();
+	potentiometer_init();
+	sei();
 
 	while (1) {
-		print_button_state();
 	}
 	return 0;
 }
 
+ISR(TIMER2_COMPA_vect) //When the interrupt triggers, updates the brightness of the LED, starts a new ADC-conversion and clears the interrupt flag.
+{
+	OCR0A = adc_value;
+	ADCSRA |= (1 << ADSC);
+	TIFR2 &= ~(1 << OCF2A);
+}
+
+ISR(ADC_vect) //When the interrupt triggers because an ADC-conversion is complete, updates the read value.
+{
+	adc_value = ADCH;
+	ADCSRA &= ~(1 << ADIF);
+}
