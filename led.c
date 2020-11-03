f87a5d096e6521702e3634e5e4a4db1025d8832b
Kettil Wigenstedt <kettil.brochmann@gmail.com>
2020-11-03 14:43:38 +0100

    Added LED_init function to led.c

--- led.c
+++ led.c
@@ -1,8 +1,11 @@
 #include <avr/io.h>
 #include "led.h"
 
-void LED_init() {
-	// ...
+#define LED_PIN 6
+#define LED_PORT PORTD
+
+void LED_init(void) {
+	DDRD |= (1 << LED_PIN);
 }
 
 uint8_t simple_ramp() {
